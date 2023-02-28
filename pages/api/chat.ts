60efe984de90a2e9ccdeda35c1317247fd5f7664
tlylt <liu_yongliang@hotmail.com>
2023-02-28 23:05:24 +0800

    update

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { ChatGPTAPI } from 'chatgpt';
+import KeyvRedis from '@keyv/redis';
+import Keyv from 'keyv';
 
 type Data = any;
 
@@ -8,11 +10,25 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  const redisUrl = process.env.REDIS_URL || 'redis://localhost:6379';
+  const store = new KeyvRedis(redisUrl);
+  const messageStore = new Keyv({ store, namespace: 'chatgpt-demo' });
   const api = new ChatGPTAPI({
     apiKey: process.env.OPENAI_API_KEY || '', // Your OpenAI API Key
+    messageStore,
   });
 
-  const openAiRes = await api.sendMessage(req.body.message);
+  let openAiRes;
+  if (req.body.conversationId && req.body.parentMessageId) {
+    const opts = {
+      conversationId: req.body.conversationId,
+      parentMessageId: req.body.parentMessageId,
+    };
+    openAiRes = await api.sendMessage(req.body.message, opts);
+  } else {
+    openAiRes = await api.sendMessage(req.body.message);
+  }
+
   res.status(200).json({
     version: 'v1',
     content: {
@@ -23,5 +39,7 @@ export default async function handler(
         },
       ],
     },
+    conversationId: openAiRes.conversationId,
+    messageId: openAiRes.id,
   });
 }
